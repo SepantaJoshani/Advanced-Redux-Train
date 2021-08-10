a610da2f4c1b01a8f807f93a24f00bbce19b9a13
Sepanta <Sepanta_97@Yahoo.com>
2021-08-10 17:08:50 +0430

    adding-ui-slice

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 
-import './index.css';
-import App from './App';
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
