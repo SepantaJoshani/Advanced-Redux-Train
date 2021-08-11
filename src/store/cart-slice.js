b2c8681b965d6a3ab021f7aa5bb207d12572e70c
Sepanta <Sepanta_97@Yahoo.com>
2021-08-11 19:21:27 +0430

    cart-slice

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,9 +1,16 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice } from '@reduxjs/toolkit';
 
 const cartSlice = createSlice({
-  name: "cart",
-  initialState: { items: [], totalQuantity: 0 },
+  name: 'cart',
+  initialState: {
+    items: [],
+    totalQuantity: 0,
+  },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
@@ -26,10 +33,9 @@ const cartSlice = createSlice({
       const existingItem = state.items.find((item) => item.id === id);
       state.totalQuantity--;
       if (existingItem.quantity === 1) {
-        state.items = state.items.filter((item) => item.id === !id);
+        state.items = state.items.filter((item) => item.id !== id);
       } else {
         existingItem.quantity--;
-        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
       }
     },
   },
