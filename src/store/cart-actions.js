bbf4b71786c06772a20937f688049385b6260f1e
Sepanta <Sepanta_97@Yahoo.com>
2021-08-13 20:15:22 +0430

    fixing-bug

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -47,7 +47,10 @@ export const sendCartData = (cart)=>{
             "https://food-order-bf696-default-rtdb.firebaseio.com/cart.json",
             {
               method: "PUT",
-              body: JSON.stringify(cart),
+              body: JSON.stringify({
+                  items:cart.items,
+                  totalQuantity:cart.totalQuantity
+              }),
             }
           );
     
