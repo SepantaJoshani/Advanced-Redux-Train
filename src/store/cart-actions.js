29662c3ad23fea8a0e4daeb7dd25c483e493b50b
Sepanta <Sepanta_97@Yahoo.com>
2021-08-13 20:00:15 +0430

    adding-cart-action

---
+++ src/store/cart-actions.js
@@ -0,0 +1,81 @@
+import { uiActions } from './ui-slice';
+import { cartActions } from './cart-slice';
+
+export const fetchCartData = ()=>{
+    return async dispatch =>{
+        const fetchData = async()=>{
+            const response = await fetch('https://food-order-bf696-default-rtdb.firebaseio.com/cart.json')
+            
+            if(!response.ok){
+                throw new Error ('Fetching Failed')
+            }
+
+            const data =await response.json()
+
+            return data
+        }
+
+        try {
+       const cartData=   await  fetchData()
+        dispatch(cartActions.replaceCart(cartData))
+        } catch (error) {
+            dispatch(
+                uiActions.showNotification({
+                  status: "error",
+                  title: "Error !",
+                  message: "Fetching Cart Data Failed !",
+                })
+              );
+        }
+    }
+}
+
+
+
+export const sendCartData = (cart)=>{
+    return async (dispatch)=>{
+       dispatch(
+          uiActions.showNotification({
+            status: "Pending",
+            title: "Sending...",
+            message: "Sending Cart Data !",
+          })
+        );
+  
+        const sendRequest = async ()=>{
+          const response = await fetch(
+            "https://food-order-bf696-default-rtdb.firebaseio.com/cart.json",
+            {
+              method: "PUT",
+              body: JSON.stringify(cart),
+            }
+          );
+    
+          if (!response.ok) {
+            throw new Error("There is An Error");
+          }
+        }
+  
+       try{
+        await sendRequest()
+        dispatch(
+          uiActions.showNotification({
+            status: "success",
+            title: "Success !",
+            message: "Sending Cart Data Successfully !",
+          })
+        );
+  
+       }catch(error){
+        dispatch(
+          uiActions.showNotification({
+            status: "error",
+            title: "Error !",
+            message: "Sending Cart Data Failed !",
+          })
+        );
+       }
+      
+       
+    }
+  }
--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -1,5 +1,4 @@
 import { createSlice } from '@reduxjs/toolkit';
-import { uiActions } from './ui-slice';
 
 const cartSlice = createSlice({
   name: 'cart',
@@ -42,53 +41,7 @@ const cartSlice = createSlice({
   },
 });
 
-export const sendCartData = (cart)=>{
-  return async (dispatch)=>{
-     dispatch(
-        uiActions.showNotification({
-          status: "Pending",
-          title: "Sending...",
-          message: "Sending Cart Data !",
-        })
-      );
 
-      const sendRequest = async ()=>{
-        const response = await fetch(
-          "https://food-order-bf696-default-rtdb.firebaseio.com/cart.json",
-          {
-            method: "PUT",
-            body: JSON.stringify(cart),
-          }
-        );
-  
-        if (!response.ok) {
-          throw new Error("There is An Error");
-        }
-      }
-
-     try{
-      await sendRequest()
-      dispatch(
-        uiActions.showNotification({
-          status: "success",
-          title: "Success !",
-          message: "Sending Cart Data Successfully !",
-        })
-      );
-
-     }catch(error){
-      dispatch(
-        uiActions.showNotification({
-          status: "error",
-          title: "Error !",
-          message: "Sending Cart Data Failed !",
-        })
-      );
-     }
-    
-     
-  }
-}
 
 
 export const cartActions = cartSlice.actions;
