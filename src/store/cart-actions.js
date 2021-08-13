b928f82a2a61408ceba6a8cfe2d8ae21aefa9f80
Sepanta <Sepanta_97@Yahoo.com>
2021-08-13 20:16:59 +0430

    clearing-cart

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -1,84 +1,86 @@
-import { uiActions } from './ui-slice';
-import { cartActions } from './cart-slice';
+import { uiActions } from "./ui-slice";
+import { cartActions } from "./cart-slice";
 
-export const fetchCartData = ()=>{
-    return async dispatch =>{
-        const fetchData = async()=>{
-            const response = await fetch('https://food-order-bf696-default-rtdb.firebaseio.com/cart.json')
-            
-            if(!response.ok){
-                throw new Error ('Fetching Failed')
-            }
+export const fetchCartData = () => {
+  return async (dispatch) => {
+    const fetchData = async () => {
+      const response = await fetch(
+        "https://food-order-bf696-default-rtdb.firebaseio.com/cart.json"
+      );
 
-            const data =await response.json()
+      if (!response.ok) {
+        throw new Error("Fetching Failed");
+      }
 
-            return data
-        }
+      const data = await response.json();
 
-        try {
-       const cartData=   await  fetchData()
-        dispatch(cartActions.replaceCart(cartData))
-        } catch (error) {
-            dispatch(
-                uiActions.showNotification({
-                  status: "error",
-                  title: "Error !",
-                  message: "Fetching Cart Data Failed !",
-                })
-              );
-        }
+      return data;
+    };
+
+    try {
+      const cartData = await fetchData();
+      dispatch(
+        cartActions.replaceCart({
+          item: cartData.items || [],
+          totalQuantity: cartData.totalQuantity,
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error !",
+          message: "Fetching Cart Data Failed !",
+        })
+      );
     }
-}
+  };
+};
 
+export const sendCartData = (cart) => {
+  return async (dispatch) => {
+    dispatch(
+      uiActions.showNotification({
+        status: "Pending",
+        title: "Sending...",
+        message: "Sending Cart Data !",
+      })
+    );
 
-
-export const sendCartData = (cart)=>{
-    return async (dispatch)=>{
-       dispatch(
-          uiActions.showNotification({
-            status: "Pending",
-            title: "Sending...",
-            message: "Sending Cart Data !",
-          })
-        );
-  
-        const sendRequest = async ()=>{
-          const response = await fetch(
-            "https://food-order-bf696-default-rtdb.firebaseio.com/cart.json",
-            {
-              method: "PUT",
-              body: JSON.stringify({
-                  items:cart.items,
-                  totalQuantity:cart.totalQuantity
-              }),
-            }
-          );
-    
-          if (!response.ok) {
-            throw new Error("There is An Error");
-          }
+    const sendRequest = async () => {
+      const response = await fetch(
+        "https://food-order-bf696-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify({
+            items: cart.items,
+            totalQuantity: cart.totalQuantity,
+          }),
         }
-  
-       try{
-        await sendRequest()
-        dispatch(
-          uiActions.showNotification({
-            status: "success",
-            title: "Success !",
-            message: "Sending Cart Data Successfully !",
-          })
-        );
-  
-       }catch(error){
-        dispatch(
-          uiActions.showNotification({
-            status: "error",
-            title: "Error !",
-            message: "Sending Cart Data Failed !",
-          })
-        );
-       }
-      
-       
+      );
+
+      if (!response.ok) {
+        throw new Error("There is An Error");
+      }
+    };
+
+    try {
+      await sendRequest();
+      dispatch(
+        uiActions.showNotification({
+          status: "success",
+          title: "Success !",
+          message: "Sending Cart Data Successfully !",
+        })
+      );
+    } catch (error) {
+      dispatch(
+        uiActions.showNotification({
+          status: "error",
+          title: "Error !",
+          message: "Sending Cart Data Failed !",
+        })
+      );
     }
-  }
+  };
+};
