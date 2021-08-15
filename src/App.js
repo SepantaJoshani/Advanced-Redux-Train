c8cf0824eca01d9f5bb4c7e9625aa3ddec8c90b3
Sepanta <Sepanta_97@Yahoo.com>
2021-08-16 00:43:48 +0430

    adding-ui-notification

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { uiActions } from "./store/ui-slice";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector(state => state.cart)
+  const dispatch = useDispatch()
 
   useEffect(()=>{
-    fetch('https://food-order-bf696-default-rtdb.firebaseio.com/',{
-      method:"PUT",
-      body:JSON.stringify(cart)
-    })
+
+    const sendData =async ()=>{
+      dispatch(uiActions.showNotification({
+        status:'Pending',
+        title:"Sending...",
+        message:"Sending Cart Data!"
+      }))
+
+
+     const response = await fetch('https://food-order-bf696-default-rtdb.firebaseio.com/cart.json',{
+        method:"PUT",
+        body:JSON.stringify(cart)
+      })
+        if(!response.ok){
+          throw new Error('Sending Data failed ...')
+        }
+      const data = response.json()
+    }
+
+    
   },[cart])
   return (
     <Layout>
